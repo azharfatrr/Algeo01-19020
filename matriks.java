f14e0e4f070efff242586f3225b0ce5eb278d331
dehanalkautsar <dehanalkautsar3@gmail.com>
2020-09-23 20:31:43 +0700

    benerin commit pertama

--- matriks.java
+++ matriks.java
@@ -2,15 +2,15 @@
 
 class matriks {
      //Atribut
-     int [][] Mat = new int[10][10]; //matriks 10x10
+     int [][] Matriks = new int[10][10]; //matriks 10x10
 
      //Method
-     matriksEmpty() { //Konstruktor
+     matriks() { //Konstruktor
           int i,j;
 
           for (i=0;i<10;i++) {
                for (j=0;j<10;j++) {
-                    this.Mat[i][j] = 0;
+                    this.Matriks[i][j] = 0;
                }
           }
      }
@@ -20,7 +20,9 @@ class matriks {
           int i,j;
           for (i=0;i<N;i++) {
                for (j=0;j<M;j++) {
-                    this.Mat[i][j] = i+j;
+                    this.Matriks[i][j] = i+j;
+               }
+          }
      }
 
      void tulisMatriks (int M, int N) {
@@ -29,7 +31,7 @@ class matriks {
 
           for (i=0;i<=M;i++) {
                for (j=0;j<=N;i++) {
-                    System.out.print(this.Mat[i][j]+" ");
+                    System.out.print(this.Matriks[i][j]+" ");
                }
                System.out.println();
           }
@@ -41,7 +43,7 @@ class matriks {
 
           for (i=0;i<=M;i++) {
                for (j=0;j<=N;j++) {
-                    Matt[j][i] = this.Mat[i][j];
+                    Matt[j][i] = this.Matriks[i][j];
                }
           }          
      }
