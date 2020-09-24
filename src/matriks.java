c6bc3d6ce57c23202e3cd8b14aed959c6ba4fa42
darubagus <darubagus@gmail.com>
2020-09-24 22:47:38 +0700

    mengubah NBrs jd NBrsEff sama NKol jd NKolEff

--- src/matriks.java
+++ src/matriks.java
@@ -4,108 +4,106 @@ class Matriks {
      /* ***** ATRIBUTE ***** */
      private int maxNBrsKol = 1; //panjang maksimum baris dan kolom matriks
      float [][] Matriks = new float[maxNBrsKol][maxNBrsKol]; //inisialisasi matriks 1x1
-     int NBrs; /* banyaknya/ukuran baris yg terdefinisi */
-	int NKol; /* banyaknya/ukuran kolom yg terdefinisi */
+     int NBrsEff; /* banyaknya/ukuran baris yg terdefinisi */
+	int NKolEff; /* banyaknya/ukuran kolom yg terdefinisi */
 
 
      /* ***** METHODS ***** */
 
      /* *** Konstruktor membentuk MATRIKS *** */
-     Matriks(int NBrs, int NKol) {
+     Matriks(final int NBrsEff, final int NKolEff) {
           // KAMUS
-          int i,j;
+          int i, j;
 
           // ALGORITMA
           // Ubah ukuran matriks
-          while(NBrs>this.maxNBrsKol || NKol>this.maxNBrsKol) {
+          while (NBrsEff > this.maxNBrsKol || NKolEff > this.maxNBrsKol) {
                this.doubleMatriks();
           }
 
           // Inisialisasi isi matriks
-          this.NBrs = NBrs;
-          this.NKol = NKol;
+          this.NBrsEff = NBrsEff;
+          this.NKolEff = NKolEff;
 
-          for (i=0;i<NBrs;i++) {
-               for (j=0;j<NKol;j++) {
+          for (i = 0; i < NBrsEff; i++) {
+               for (j = 0; j < NKolEff; j++) {
                     this.Matriks[i][j] = 0;
                }
           }
-              
+
      }
 
      /* *** KELOMPOK BACA/TULIS *** */
-     /** Baca Matriks
-      * I.S. Membaca matriks pada layar
-      * F.S. Matriks terdefinisi nilai elemen efektifnya, berukuran NB x NK
+     /**
+      * Baca Matriks I.S. Membaca matriks pada layar F.S. Matriks terdefinisi nilai
+      * elemen efektifnya, berukuran NB x NK
       */
-     void bacaMatriks () {
-          int i,j;
-          Scanner input = new Scanner (System.in);
+     void bacaMatriks() {
+          int i, j;
+          final Scanner input = new Scanner(System.in);
 
-          for (i=0;i < this.NBrs;i++) {
-               for (j=0;j < this.NKol;j++) {
+          for (i = 0; i < this.NBrsEff; i++) {
+               for (j = 0; j < this.NKolEff; j++) {
                     this.Matriks[i][j] = input.nextFloat();
                }
           }
      }
 
-     /** Tulis Matriks
-      * I.S. Matriks terdefinisi dan memiliki nilai
-      * F.S. Menampilkan matriks pada layar
+     /**
+      * Tulis Matriks I.S. Matriks terdefinisi dan memiliki nilai F.S. Menampilkan
+      * matriks pada layar
       */
-     void tulisMatriks () {
+     void tulisMatriks() {
           // mencetak elemen-elemen matriks hingga indeks M,N
-          int i,j;
+          int i, j;
 
-          for (i=0;i < this.NBrs;i++) {
-               for (j=0;j < this.NKol;j++) {
-                    System.out.print(this.Matriks[i][j]+" ");
+          for (i = 0; i < this.NBrsEff; i++) {
+               for (j = 0; j < this.NKolEff; j++) {
+                    System.out.print(this.Matriks[i][j] + " ");
                }
                System.out.println();
           }
      }
 
-     void transpose (int M, int N, float Matt[][]) {
-          //Membuat matriks transpose
-          int i,j;
+     void transpose(final int M, final int N, final float Matt[][]) {
+          // Membuat matriks transpose
+          int i, j;
 
-          for (i=0;i<=M;i++) {
-               for (j=0;j<=N;j++) {
+          for (i = 0; i <= M; i++) {
+               for (j = 0; j <= N; j++) {
                     Matt[j][i] = this.Matriks[i][j];
                }
-          }          
+          }
      }
-
 
      /* *** MEMORY MANAGEMENT *** */
 
-     /** Mengubah ukuran maksimum Baris dan Kolom Matriks menjadi 2x semula
-      * I.S. Matriks Terdefinisi
-      * F.S. Panjang maksimum Baris dan Kolom Matriks menjadi 2x semula
-      *      Ukuran memory matriks menjadi 4x semula 
-      **/ 
-     void doubleMatriks()
-     {
+     /**
+      * Mengubah ukuran maksimum Baris dan Kolom Matriks menjadi 2x semula I.S.
+      * Matriks Terdefinisi F.S. Panjang maksimum Baris dan Kolom Matriks menjadi 2x
+      * semula Ukuran memory matriks menjadi 4x semula
+      **/
+     void doubleMatriks() {
           // Kamus
-          this.maxNBrsKol = this.maxNBrsKol*2;
-          float [][] NewMatriks = new float[this.maxNBrsKol][this.maxNBrsKol];
+          this.maxNBrsKol = this.maxNBrsKol * 2;
+          float[][] NewMatriks = new float[this.maxNBrsKol][this.maxNBrsKol];
 
           // Algoritma
           NewMatriks = this.copyArrayMatriks();
           this.Matriks = NewMatriks;
      }
 
-     /** Mengubah ukuran maksimum Baris dan Kolom Matriks menjadi 2x semula
-      * I.S. Matriks Terdefinisi dan panjang baris dan kolom maksumim 2x NBrs dan NKol
-      * F.S. Panjang maksimum Baris dan Kolom Matriks menjadi 1/2x semula
-      *      Ukuran memory matriks menjadi 1/4x semula 
-      **/ 
-     void halfMatriks()
-     {
-          if ((this.NBrs <= this.maxNBrsKol/2) && (this.NKol <= this.maxNBrsKol/2)) {
+     /**
+      * Mengubah ukuran maksimum Baris dan Kolom Matriks menjadi 2x semula I.S.
+      * Matriks Terdefinisi dan panjang baris dan kolom maksumim 2x NBrsEff dan NKolEff
+      * F.S. Panjang maksimum Baris dan Kolom Matriks menjadi 1/2x semula Ukuran
+      * memory matriks menjadi 1/4x semula
+      **/
+     void halfMatriks() {
+          if ((this.NBrsEff <= this.maxNBrsKol / 2) && (this.NKolEff <= this.maxNBrsKol / 2)) {
                // Kamus
-               this.maxNBrsKol = this.maxNBrsKol*1/2;
-               float [][] NewMatriks = new float[this.maxNBrsKol][this.maxNBrsKol];
+               this.maxNBrsKol = this.maxNBrsKol * 1 / 2;
+               float[][] NewMatriks = new float[this.maxNBrsKol][this.maxNBrsKol];
 
                // Algoritma
                NewMatriks = this.copyArrayMatriks();
@@ -116,30 +114,28 @@ class Matriks {
      }
 
      /** Mengcopy array matriks ke tempat lain */
-     float[][] copyArrayMatriks()
-     {
+     float[][] copyArrayMatriks() {
           // Kamus
-          float [][] MHsl = new float[this.maxNBrsKol][this.maxNBrsKol];
+          final float[][] MHsl = new float[this.maxNBrsKol][this.maxNBrsKol];
 
           // Algoritma
-          for (int i = 0; i < this.NBrs; i++) {
-               for (int j = 0; j < this.NBrs; j++) {
+          for (int i = 0; i < this.NBrsEff; i++) {
+               for (int j = 0; j < this.NBrsEff; j++) {
                     MHsl[i][j] = this.Matriks[i][j];
                }
           }
-          return MHsl; 
+          return MHsl;
      }
 
      /** Mencopy matriks ke matriks lain */
-     Matriks copyMatriks()
-     {
+     Matriks copyMatriks() {
           // Kamus
-          Matriks MCopy = new Matriks(this.NBrs, this.NKol);
+          final Matriks MCopy = new Matriks(this.NBrsEff, this.NKolEff);
           // Algoritma
           MCopy.Matriks = this.copyArrayMatriks();
           MCopy.maxNBrsKol = this.maxNBrsKol;
-          MCopy.NBrs = this.NBrs;
-          MCopy.NKol = this.NKol;
+          MCopy.NBrsEff = this.NBrsEff;
+          MCopy.NKolEff = this.NKolEff;
 
           return MCopy;
      }
