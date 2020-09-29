185afc747096a3c15f9dede367e0c3c135fbe688
darubagus <darubagus@gmail.com>
2020-09-29 21:11:42 +0700

    Invers Gauss-Jordan dan Determinan Gauss
    UDAH FIX YEY

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,27 @@ public class Main {
 
 
         // Regresi
-        Regresi M = new Regresi(0, 0);
+        /*Regresi M = new Regresi(0, 0);
 
         M.bacaRegresi();
         M.tulisRegresi();
 
-        M.tulisFileRegresi("hasilRegresi.txt");
+        M.tulisFileRegresi("hasilRegresi.txt");*/
+        Matriks M = new Matriks(0,0);
+
+        Matriks Inv = new Matriks(0,0);
+
+        M.bacaFileMatriks("cek3.txt");
+
+        float Det;
+        Det = M.DeterminanKofaktor();
+        System.out.println(Det);
+        
+        Det = M.DeterminanGauss();
+        
+        System.out.println(Det);
+        
+
 
 
 
